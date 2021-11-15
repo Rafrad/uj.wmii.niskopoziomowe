db2fbc1a2ed477d4b1afcb7060d520212f861ed9
root <root@DESKTOP-SK8MVTJ.localdomain>
2021-11-16 00:52:49 +0100

    5\4

--- fpu/fpu4.asm
+++ fpu/fpu4.asm
@@ -15,7 +15,9 @@
 
  push ebp
  mov ebp, esp
- push ebx                 ; KONWENCJA C - musimy zachować oryginalne dane z rejestru EBX
+ push eax
+ push ebx
+ push ecx                 ; KONWENCJA C - musimy zachować oryginalne dane z rejestru EBX
 
  mov eax, wartosci
  mov ecx, k
@@ -24,6 +26,8 @@
  fld xmin
  fsubp ST1     ; stack: xmax-xmin
  fild k         ; stack; k, xmax-xmin
+ fld1
+ fsubp ST1     ; stack: k-1, xmax-xmin
  fdivp ST1     ; stack: interval
 
  fld xmin      ; stack: x, int
@@ -37,15 +41,15 @@
  fld P
  fmul ST1        ; stack: P*2*pi*x, 2*pi*x, x, int
  fsin            ; stack: sin(P*2pix), 2pix, x, int
- fmul ST0        ; stack: sin(P*2pix)^2, 2pix, x, int
- fld a           ; stack: a, ...
+ fld a           ; stack: a, sin(P*2pix), 2pix, x, int
+ fmul ST1        ; stack: a*sin(...), sin(P*2pix), 2pix, x, int
  fmulp ST1       ; stack: a*..., 2pix, x, int
  
  fld Q
  fmul ST2        ; stack: Q*2pix, a*..., 2pix, x, int
  fsin            ; stack: sin(Q*2pix), a*..., 2pix, x, int
- fmul ST0        ; stack: sin(Q*2pix)^2, a*..., 2pix, x, int
- fld b
+ fld b 
+ fmul ST1        ; stack: b*sin(...), sin(Q*2pix), a*..., 2pix, x, int
  fmulp ST1       ; stack: b*..., a*..., 2pix, x, int
 
  faddp ST1       ; stack: a*...+b*..., 2pix, x, int
@@ -56,7 +60,9 @@
  loop while
  
  quit:
+ pop ecx
  pop ebx
+ pop eax
  mov esp, ebp
  pop ebp
  ret
